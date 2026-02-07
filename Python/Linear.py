19bb546e0f612dbdec701da90c5200768deb2f45
Ahmed-afk7766 <nadeemmaheen770@gmail.com>
2026-02-07 17:34:50 +0500

    Sqaure Time

---
+++ Python/Linear.py
@@ -0,0 +1,13 @@
+def Ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("When n is ",n," Iterations = ",iteration)
+
+
+Ontime(10)
+Ontime(20)
+Ontime(40)
+
+
+print("\nWith every 'n' the time taken and iterations will increase ")
