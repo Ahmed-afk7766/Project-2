aa6d8f5f356fcbbd3056857cec5f0e78264689b1
Ahmed-afk7766 <nadeemmaheen770@gmail.com>
2026-03-01 17:24:54 +0500

    Set Bit

---
+++ Python/nthBitsetorNot.py
@@ -0,0 +1,11 @@
+def setorNot(number, n):
+
+    if number & (1 << (n - 1)):
+        print( "\nSET")
+    else:
+        print("\nNOT SET")
+
+
+number = int(input("Enter your number :"))
+n = int(input("Enter bit number :"))
+setorNot(number, n)
